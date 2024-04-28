bbe2e04fafd2212d138e3e77c53df3204bcde92b
nikoduque-doo <98504134+nikoduque-doo@users.noreply.github.com>
2024-04-29 00:27:42 +0200

    Added HeightMap.java file and its classes

---
+++ HeightMap.java
@@ -0,0 +1,72 @@
+import java.util.concurrent.TimeUnit;
+
+public class HeightMap {
+    public static void main(String[] args){
+        System.out.println("hey");
+        SquareDiamond(1);
+    }
+
+    public static void SquareDiamond(int n){
+        //Create array of valid size
+        int size = (int)Math.pow(2, n) + 1;
+        float[][] heightMapArray = new float[size][size];
+
+        //Initialize corner values
+        heightMapArray[0][0] = 1.0f;
+        heightMapArray[0][size - 1] = 2.0f;
+        heightMapArray[size - 1][0] = 3.0f;
+        heightMapArray[size - 1][size - 1] = 4.0f;
+
+        int step = (int)Math.floor(size/2);
+
+        //Iterate over n
+        for(int it = 0; it < n; it++){
+            Square(heightMapArray, step);
+            Diamond(heightMapArray, step);
+            PrintArray(heightMapArray);
+            System.out.println();
+            try{Thread.sleep(2000);} catch (Exception e) {}
+            step /= 2;
+        }
+    }
+
+    public static void Square(float[][] arr, int step){
+        //iterate over top-left corners
+        for(int i = 0; i < arr.length - 1; i+= 2 * step){
+            for(int j = 0; j < arr[0].length - 1; j += 2 * step){
+                //find center of square
+                arr[i + step][j + step] = (
+                    arr[i][j] + arr[i + step * 2][j] + arr[i][j + step * 2] + arr[i + step * 2][j + step * 2]) / 4;
+            }
+        }
+    }
+
+    public static void Diamond(float[][] arr, int step){
+        //iterate over the diamond centers
+        for(int i = 0; i < arr.length; i+= step){
+            //alternating between starting on index 0 or index step
+            int alt_start = (i + step) % (2 * step);
+            for(int j = alt_start; j < arr[0].length; j += 2 * step){
+                float sum = 0;
+                float num_el = 0;
+                if(i - step >= 0){sum += arr[i - step][j]; num_el += 1;}
+                if(i + step < arr.length){sum += arr[i + step][j]; num_el += 1;}
+                if(j - step >= 0){sum += arr[i][j - step]; num_el += 1;}
+                if(j + step < arr.length){sum += arr[i][j + step]; num_el += 1;}
+                arr[i][j] = sum / num_el;
+            }
+        }
+    }
+
+    public static void PrintArray(float[][] arr){
+        for(int i = 0 ; i < arr.length; i++){
+            for(int j = 0 ; j < arr[0].length; j++){
+                System.out.print(String.format("%.4f", arr[i][j]));
+                if(j < arr.length - 1){
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}

3e31f26a54dcd36a964fd419b0be7df0b16c4c7d
nikoduque-doo <98504134+nikoduque-doo@users.noreply.github.com>
2024-04-30 23:18:38 +0200

    Refined toMesh class

---
+++ toMesh.java
@@ -0,0 +1,75 @@
+public class toMesh {
+
+    public static void main(String[] args) {
+        double[][] arr = HeightMap.DiamondSquare(3, 0.5);
+        getMesh(arr, 1, 1, 1);
+    }
+
+    public static record Mesh(double[][] vertices, int[][] faces){};
+
+    public static Mesh getMesh(double[][] arr, double scaleX, double scaleY, double scaleZ){
+        int[][] faces = getTriangleMatrix(arr);
+        double[][] vertices = getVertexMatrix(arr, scaleX, scaleY, scaleZ);
+
+        return new Mesh(vertices, faces);
+    }
+
+    public static int[][] getTriangleMatrix(double[][] hMap){
+        //Number of Triangles is two times the number of 2*2 squares
+        int size = 2 *(hMap.length-1) * (hMap.length-1);
+
+        //Matrix with 3 vertex indexes per triangle
+        int[][] triMatrix = new int[size][3];
+        int triCounter = 0;
+
+        for(int i = 0; i < hMap.length - 1; i++){
+            for(int j = 0; j < hMap.length - 1; j++){
+                int topLeftVertexIndex = i * hMap.length + j + 1;
+
+                int[] vertexMatrixA = {
+                    topLeftVertexIndex, 
+                    topLeftVertexIndex + 1, 
+                    topLeftVertexIndex + hMap.length
+                };
+
+                int[] vertexMatrixB = {
+                    topLeftVertexIndex + 1, 
+                    topLeftVertexIndex + hMap.length, 
+                    topLeftVertexIndex + hMap.length + 1
+                };
+
+
+                triMatrix[triCounter] = vertexMatrixA;
+                triMatrix[triCounter + 1] = vertexMatrixB;
+
+                triCounter += 2;
+
+            }
+        }
+
+        return triMatrix;
+    }
+
+    public static double[][] getVertexMatrix(double[][] arr, double scaleX, double scaleY, double scaleZ){
+        
+        double[][] vertexMatrix = new double[arr.length * arr.length][3];
+        int vertexCount = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr.length; j++){
+                double[] vertexCoordinates = {
+                    scaleX * i,
+                    scaleY * j,
+                    scaleZ * arr[i][j]
+                };
+
+                vertexMatrix[vertexCount] = vertexCoordinates;
+                vertexCount++;
+            }
+        }
+
+        PrintingUtilities.PrintArray(arr);
+        PrintingUtilities.PrintArray(vertexMatrix);
+        return vertexMatrix;
+    }
+}

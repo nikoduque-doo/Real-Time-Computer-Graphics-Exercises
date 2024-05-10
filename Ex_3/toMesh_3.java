200b2aac2eec1df0d33a6de896f931237aa6630d
nikoduque-doo <98504134+nikoduque-doo@users.noreply.github.com>
2024-05-10 23:14:13 +0200

    Task 3 V. 1.0

---
+++ Ex_3/toMesh_3.java
@@ -0,0 +1,131 @@
+public class toMesh_3 {
+
+    public static record Mesh(double[][] vertices, int[][] faces, double[][] normals, double[][] textureCoordinates){};
+
+    public static Mesh getMesh(double[][] arr, double scaleX, double scaleY, double scaleZ){
+        int[][] faces = getTriangleMatrix(arr);
+        double[][] vertices = getVertexMatrix(arr, scaleX, scaleY, scaleZ);
+        double[][] normals = getNormalMatrix(arr, scaleX, scaleY);
+        double[][] textureCoordinates = getTextureCoordinates(arr, scaleX, scaleY);
+
+        return new Mesh(vertices, faces, normals, textureCoordinates);
+    }
+
+    public static int[][] getTriangleMatrix(double[][] hMap){
+        //Number of Triangles is two times the number of 2*2 squares
+        int size = 2 *(hMap.length-1) * (hMap.length-1);
+
+        //Matrix with 3 vertex indexes per triangle
+        int[][] triMatrix = new int[size][3];
+        int triCounter = 0;
+
+        for(int i = 0; i < hMap.length - 1; i++){
+            for(int j = 0; j < hMap.length - 1; j++){
+                int topLeftVertexIndex = i * hMap.length + j + 1;
+
+                int[] vertexMatrixA = {
+                    topLeftVertexIndex, 
+                    topLeftVertexIndex + 1, 
+                    topLeftVertexIndex + hMap.length
+                };
+
+                int[] vertexMatrixB = {
+                    topLeftVertexIndex + 1, 
+                    topLeftVertexIndex + hMap.length, 
+                    topLeftVertexIndex + hMap.length + 1
+                };
+
+
+                triMatrix[triCounter] = vertexMatrixA;
+                triMatrix[triCounter + 1] = vertexMatrixB;
+
+                triCounter += 2;
+
+            }
+        }
+
+        return triMatrix;
+    }
+
+    public static double[][] getVertexMatrix(double[][] arr, double scaleX, double scaleY, double scaleZ){
+        
+        double[][] vertexMatrix = new double[arr.length * arr.length][3];
+        int vertexCount = 0;
+        double scale = arr.length;
+
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr.length; j++){
+                double[] vertexCoordinates = {
+                    scaleX / scale * j,
+                    scaleY / scale * i,
+                    scaleZ * arr[i][j]
+                };
+
+                vertexMatrix[vertexCount] = vertexCoordinates;
+                vertexCount++;
+            }
+        }
+
+        return vertexMatrix;
+    }
+
+    public static double[][] getNormalMatrix(double[][] arr, double scaleX, double scaleY){
+        double[][] normals = new double[arr.length * arr.length][3];
+        int vertexCount = 0;
+        double scale = arr.length;
+
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr.length; j++){
+
+                //Avoids if statements, edge cases utilize the existing vertices and the evaluated vertex
+                int left = j > 0 ? -1 : 0;
+                int right = j < arr.length - 1 ? 1 : 0;
+                int up = i > 0 ? -1 : 0;
+                int down = i < arr.length - 1 ? 1 : 0;
+
+                //inverse scale
+                double[] vertexNormals = {
+                    scale/scaleX * (arr[i][j + right] - arr[i][j + left]) / (right - left),
+                    scale/scaleY * (arr[i + down][j] - arr[i + up][j]) / (down - up),
+                    1
+                };
+
+                normalizeVector(vertexNormals);
+
+                normals[vertexCount] = vertexNormals;
+                vertexCount++;
+            }
+        }
+        
+        return normals;
+    }
+
+    public static double[][] getTextureCoordinates(double[][] arr, double scaleX, double scaleY){
+        double[][] textCoord = new double[arr.length * arr.length][2];
+        int vertexCount = 0;
+        double scale = arr.length;
+
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr.length; j++){
+                textCoord[vertexCount] = new double[]{(double) i/scale, (double) j/scale};
+                vertexCount++;
+            }
+        }
+        
+        return textCoord;
+    }
+
+    public static void normalizeVector(double[] vector){
+        double norm = 0;
+
+        for(int i = 0; i < vector.length; i++){
+            norm += vector[i] * vector[i];
+        }
+
+        norm = Math.sqrt(norm);
+
+        for(int i = 0; i < vector.length; i++){
+            vector[i] /= norm;
+        }
+    }
+}

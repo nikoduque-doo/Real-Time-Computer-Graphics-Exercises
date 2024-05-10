3dae06ede17de8a7eb9171f08596b9ad34f85639
nikoduque-doo <98504134+nikoduque-doo@users.noreply.github.com>
2024-05-10 23:23:26 +0200

    Task 3 V.2

--- Ex_3/toMesh_3.java
+++ Ex_3/toMesh_3.java
@@ -4,11 +4,53 @@ public class toMesh_3 {
 
     public static Mesh getMesh(double[][] arr, double scaleX, double scaleY, double scaleZ){
         int[][] faces = getTriangleMatrix(arr);
-        double[][] vertices = getVertexMatrix(arr, scaleX, scaleY, scaleZ);
-        double[][] normals = getNormalMatrix(arr, scaleX, scaleY);
-        double[][] textureCoordinates = getTextureCoordinates(arr, scaleX, scaleY);
+        // double[][] vertices = getVertexMatrix(arr, scaleX, scaleY, scaleZ);
+        // double[][] normals = getNormalMatrix(arr, scaleX, scaleY);
+        // double[][] textureCoordinates = getTextureCoordinates(arr, scaleX, scaleY);
+        double[][] vertexMatrix = new double[arr.length * arr.length][3];
+        double[][] normals = new double[arr.length * arr.length][3];
+        double[][] textCoord = new double[arr.length * arr.length][2];
 
-        return new Mesh(vertices, faces, normals, textureCoordinates);
+        int vertexCount = 0;
+        double scale = arr.length;
+
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr.length; j++){
+                //get Vertex Coordinates
+                double[] vertexCoordinates = {
+                    scaleX / scale * j,
+                    scaleY / scale * i,
+                    scaleZ * arr[i][j]
+                };
+
+                vertexMatrix[vertexCount] = vertexCoordinates;
+
+                //Get Vertex Normals
+                //Avoids if statements, edge cases utilize the existing vertices and the evaluated vertex
+                int left = j > 0 ? -1 : 0;
+                int right = j < arr.length - 1 ? 1 : 0;
+                int up = i > 0 ? -1 : 0;
+                int down = i < arr.length - 1 ? 1 : 0;
+
+                //inverse scale
+                double[] vertexNormals = {
+                    scale/scaleX * (arr[i][j + right] - arr[i][j + left]) / (right - left),
+                    scale/scaleY * (arr[i + down][j] - arr[i + up][j]) / (down - up),
+                    1
+                };
+
+                normalizeVector(vertexNormals);
+
+                normals[vertexCount] = vertexNormals;
+
+                //Get texture coordinates
+                textCoord[vertexCount] = new double[]{(double) i/scale, (double) j/scale};
+
+                vertexCount++;
+            }
+        }
+
+        return new Mesh(vertexMatrix, faces, normals, textCoord);
     }
 
     public static int[][] getTriangleMatrix(double[][] hMap){
@@ -47,74 +89,7 @@ public class toMesh_3 {
         return triMatrix;
     }
 
-    public static double[][] getVertexMatrix(double[][] arr, double scaleX, double scaleY, double scaleZ){
-        
-        double[][] vertexMatrix = new double[arr.length * arr.length][3];
-        int vertexCount = 0;
-        double scale = arr.length;
-
-        for(int i = 0; i < arr.length; i++){
-            for(int j = 0; j < arr.length; j++){
-                double[] vertexCoordinates = {
-                    scaleX / scale * j,
-                    scaleY / scale * i,
-                    scaleZ * arr[i][j]
-                };
-
-                vertexMatrix[vertexCount] = vertexCoordinates;
-                vertexCount++;
-            }
-        }
-
-        return vertexMatrix;
-    }
-
-    public static double[][] getNormalMatrix(double[][] arr, double scaleX, double scaleY){
-        double[][] normals = new double[arr.length * arr.length][3];
-        int vertexCount = 0;
-        double scale = arr.length;
-
-        for(int i = 0; i < arr.length; i++){
-            for(int j = 0; j < arr.length; j++){
-
-                //Avoids if statements, edge cases utilize the existing vertices and the evaluated vertex
-                int left = j > 0 ? -1 : 0;
-                int right = j < arr.length - 1 ? 1 : 0;
-                int up = i > 0 ? -1 : 0;
-                int down = i < arr.length - 1 ? 1 : 0;
-
-                //inverse scale
-                double[] vertexNormals = {
-                    scale/scaleX * (arr[i][j + right] - arr[i][j + left]) / (right - left),
-                    scale/scaleY * (arr[i + down][j] - arr[i + up][j]) / (down - up),
-                    1
-                };
-
-                normalizeVector(vertexNormals);
-
-                normals[vertexCount] = vertexNormals;
-                vertexCount++;
-            }
-        }
-        
-        return normals;
-    }
-
-    public static double[][] getTextureCoordinates(double[][] arr, double scaleX, double scaleY){
-        double[][] textCoord = new double[arr.length * arr.length][2];
-        int vertexCount = 0;
-        double scale = arr.length;
-
-        for(int i = 0; i < arr.length; i++){
-            for(int j = 0; j < arr.length; j++){
-                textCoord[vertexCount] = new double[]{(double) i/scale, (double) j/scale};
-                vertexCount++;
-            }
-        }
-        
-        return textCoord;
-    }
-
+    
     public static void normalizeVector(double[] vector){
         double norm = 0;
 
